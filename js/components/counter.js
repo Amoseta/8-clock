a5cdaa93c6069bec9456f034d3da5f9a8f67a9c0
Renata <76951689+Amoseta@users.noreply.github.com>
2021-02-09 10:00:51 +0200

    Set interval and DOM

--- js/components/counter.js
+++ js/components/counter.js
@@ -6,9 +6,48 @@ reikalavimai:
 - pasiekus nuli - parasyti FINITO
 */
 
+/*let number = 0; - global JS
+
 function counter(selector) {
     const DOM = document.querySelector(selector);
-    console.log(DOM);
+    const number = parseFloat(DOM.innerText);
+
+    setTimeout(showMeNumber, 3000);
+}
+function showMeNumber() {
+    console.log(number);
+}
+
+
+export { counter }*/
+
+/* function counter(selector) {
+    const DOM = document.querySelector(selector);
+    const number = parseInt(DOM.innerText);
+
+    setTimeout(function () {
+        showMeNumber(number);
+    },3000);
+}
+function showMeNumber(num) {
+    console.log(num);
+}
+
+
+export { counter }*/
+
+function counter(selector) {
+    const DOM = document.querySelector(selector);
+    let number = parseInt(DOM.innerText);
+
+    const timer = setInterval(function () {
+        DOM.innerText = number;
+        number--;
+        if (number < 0) {
+            clearInterval(timer);
+            DOM.innerText = 'FINITO';
+        }
+    }, 500);
 }
 
 export { counter }

e146d3866c207f4a5ab7f24815a3f263db180f34
Renata <76951689+Amoseta@users.noreply.github.com>
2021-02-09 09:01:51 +0200

    Initial commit

---
+++ js/components/counter.js
@@ -0,0 +1,14 @@
+/*
+reikalavimai:
+- rasti vieta/elementa;
+- istraukti skaiciu;
+- kas sekunde ta skaiciu mazinti iki nulio;
+- pasiekus nuli - parasyti FINITO
+*/
+
+function counter(selector) {
+    const DOM = document.querySelector(selector);
+    console.log(DOM);
+}
+
+export { counter }
